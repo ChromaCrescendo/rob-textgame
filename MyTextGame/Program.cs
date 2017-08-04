4ff60f29280539401de703542a74db924e502257
unknown <Robert>
2017-08-04 01:38:51 -0400

    Added three class system with armor damage reduction, and new dialogue

--- MyTextGame/Program.cs
+++ MyTextGame/Program.cs
@@ -9,20 +9,13 @@ namespace MyTextGame
     class Character
     {
         public int hp;
-        //public int mp;
+        public int mp;
         public string name;
-        public int minAtk;
+        public int minAtk = 1;
         public int maxAtk;
         //public int ac;
         public int dmg;
-
-        /*stats unimplimented
-        public int str;
-        public int dex;
-        public int con;
-        public int intel;
-        public int wis;
-        public int cha;*/
+        public int armor;
 
 
 
@@ -72,6 +65,41 @@ namespace MyTextGame
 
     }
 
+    class Paladin : Character
+    {
+        public Paladin()
+        {
+            hp = 70;
+            maxAtk = 7;
+            armor = 7;
+            mp = 10;
+        }
+        
+    }
+
+    class Mage: Character
+    {
+        public Mage()
+        {
+            hp = 30;
+            maxAtk = 11;
+            armor = 0;
+            mp = 20;
+                
+        }
+    }
+
+    class Warrior: Character
+    {
+        public Warrior()
+        {
+            hp = 50;
+            maxAtk = 9;
+            armor = 5;
+            mp = 0;
+        }
+    }
+
 
 
     class Program
@@ -81,12 +109,6 @@ namespace MyTextGame
             //intial setup for random damage throw
             Random r = new Random();
 
-            //setting up player one starting stats
-            Character p1 = new Character();
-            p1.hp = 50;
-            p1.minAtk = 1;
-            p1.maxAtk = 10;
-
             //setting up first monster
             Character m1 = new Character();
             m1.hp = 30;
@@ -94,16 +116,73 @@ namespace MyTextGame
             m1.maxAtk = 5;
             m1.name = "Large Rat";
 
-            //bool namegood = false;
-
             //appearance of first NPC and player name entry
-            Console.WriteLine("You awake in a cage in the dark. You hear a rough voice ask, 'What be your name lad?'");
+            Character p1;
+            Console.WriteLine("You awake in a cage in the dark. You hear a rough voice ask, 'Stranger, what is it that you do?'");     
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine("You look all around you but see no one, only bars of iron\n");
+            System.Threading.Thread.Sleep(1000);
+            //bool behindBars = true;
+            bool validClass = false;
+            string classInput;
+            string classInOriginal;
+            do
+            {
+                Console.WriteLine("Pick a class: paladin, warrior, mage");
+
+                classInOriginal = Console.ReadLine();
+                classInput = classInOriginal.ToUpper();
+                switch (classInput)
+                    {
+                        case "PALADIN":
+                            p1 = new Paladin();
+                            validClass = true;
+                            break;
+                        case "WARRIOR":
+                            p1 = new Warrior();
+                            validClass = true;
+                            break;
+                        case "MAGE":
+                            p1 = new Mage();
+                            validClass = true;
+                            break;
+                        default:
+                            p1 = new Character();
+                            validClass = false;
+                            Console.WriteLine("'I'm sorry, you were a what now?', queries the voice, bewildered.");
+                            Console.WriteLine("(Invalid Class. Try Again!)");
+                            break;
+                    }
+                
+            } while (validClass == false);
+
+            //figured out a clever way to utilize the to upper function on chosen class, thought the solution was funny
+            //but also figured out how to keep and use the original case, but kept the to upper plot utilization for the development of the npc character
+
+            Console.WriteLine("'BUT OF COURSE... YOU WERE A {0}!', exclaims the voice.", classInput);
+            //epic pauses to harness the true awkward silence
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine("'SHUT THE @*&# UP! OR I'LL BEAT YOU TO SLEEP!', you hear a voice shout in the distance");
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine("'...'");
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine("'I apologize, I... ");
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine("'I have random outbursts when I get excited'");
+            System.Threading.Thread.Sleep(2000);
+            Console.WriteLine("'Now what did you say your name was, young {0}?'", classInOriginal);
+
             p1.name = Console.ReadLine();
             Console.WriteLine("{0}? What a terrible name!", p1.name);
+            System.Threading.Thread.Sleep(500);
+            Console.WriteLine("{0} the {1}? No that doesn't have a good ring to it... How did you end up in this prison?", p1.name, classInOriginal);
+            Console.ReadLine();
+            Console.WriteLine("'Oh wow really? That's interesting... Just kidding I don't care.");
             System.Threading.Thread.Sleep(1000);
             Console.WriteLine("'Shhhh be quiet {0}!', whispers the voice, 'Is that a rat?! Kill it before you catch a disease!'", p1.name);
-            //Console.ForegroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("A " + m1.name + " appears before you with " + m1.hp + " health and " + m1.maxAtk + " attack");
+            Console.ForegroundColor = ConsoleColor.Gray;
             System.Threading.Thread.Sleep(500);
             Console.WriteLine("What do you do? Attack or run?");
 
@@ -127,7 +206,8 @@ namespace MyTextGame
                         break;
 
                     case "RUN":
-                        Console.WriteLine("You cannot run you are in a cage");
+
+                        Console.WriteLine("You attempt to flee but you are behind bars, so you run in a circle");
                         break;
 
                     default:
@@ -135,12 +215,27 @@ namespace MyTextGame
                         break;
 
                 }
-
+                //checking if the monster is still alive, without this the monster will deal a final blow with negative health
                 if (m1.hp > 0)
                 {
                     Console.ForegroundColor = ConsoleColor.Red;
                     Console.WriteLine("The " + m1.name + " attacks you for " + m1.dmg + " damage");
-                    p1.hp -= m1.dmg;
+                    //double nested if statement checks if you're a mage to do armor calculations
+                    if (classInput != "MAGE")
+                    {
+                        Console.WriteLine("Your worn armor saves you from " + p1.armor + " damage");
+                        //this triple nested if statement prevents an enemy from dealing negative damage because of armor and healing the character (HOPEFULLY)
+                        if (m1.dmg > p1.armor)
+                        {
+                            p1.hp -= (m1.dmg - p1.armor);
+                        }
+                        else
+                        {
+                            Console.WriteLine("The attack bounces off your armor and you receive no damage");
+                        }
+                    }
+                    else { p1.hp -= m1.dmg; }
+                        
                     Console.ForegroundColor = ConsoleColor.Green;
                     Console.WriteLine("You now have " + p1.hp + " health left \n");
                     //Console.WriteLine();
@@ -161,8 +256,7 @@ namespace MyTextGame
             else
             {
                 Console.WriteLine("YOU WIN");
-                Console.WriteLine("'Very good me lad, {0}, you were named poorly but ye be quite scrappy! Now make a lockpick and get us out of here', " +
-                    "says the voice", p1.name);
+                Console.WriteLine("'Very good me lad, {0}, you were named poorly but ye be quite scrappy!", p1.name);
             }
 
             Console.ReadKey();
