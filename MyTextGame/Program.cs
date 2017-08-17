7372b7093d44a463d5bf5a94f18d88fb4c5658f2
unknown <robert.ross.iv@icloud.com>
2017-08-17 02:51:43 -0400

    added concept of max health and current health, implemented, minor health info display adjustments and victory\defeat messages

--- MyTextGame/Program.cs
+++ MyTextGame/Program.cs
@@ -4,7 +4,9 @@ namespace MyTextGame
 {
     class Character
     {
-        public int hp; public int mp;
+        public int hpCurrent;
+        public int hpMax;
+        public int mp;
         public string name;
         public int minAtk = 1;
         public int maxAtk;
@@ -21,7 +23,7 @@ namespace MyTextGame
     {
         public Paladin()
         {
-            hp = 70;
+            hpMax = 70;
             maxAtk = 7;
             armor = 5;
             mp = 10;
@@ -33,7 +35,7 @@ namespace MyTextGame
     {
         public Mage()
         {
-            hp = 30;
+            hpMax = 30;
             maxAtk = 11;
             armor = 0;
             mp = 20;
@@ -44,7 +46,7 @@ namespace MyTextGame
     {
         public Warrior()
         {
-            hp = 50;
+            hpMax = 50;
             minAtk = 3;
             maxAtk = 9;
             armor = 3;
@@ -52,6 +54,7 @@ namespace MyTextGame
         }
     }
 
+ 
     class Battle
     {
         public Character p1;
@@ -60,13 +63,13 @@ namespace MyTextGame
         public bool doBattle()
         {
             Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("A " + m1.name + " appears before you with " + m1.hp + " health and " + m1.maxAtk-- + " attack");
+            Console.WriteLine("A " + m1.name + " appears before you with " + m1.hpCurrent + " health and " + m1.maxAtk-- + " attack");
             Console.ForegroundColor = ConsoleColor.Gray;
             System.Threading.Thread.Sleep(1000);
             Console.WriteLine("What do you do? Attack or run?");
             System.Threading.Thread.Sleep(1000);
 
-            while (p1.hp > 0 && m1.hp > 0)
+            while (p1.hpCurrent > 0 && m1.hpCurrent > 0)
             {
 
                 //secondary setup for
@@ -82,10 +85,10 @@ namespace MyTextGame
                     case "ATTACK":
                     case "ATK":
                         Console.WriteLine("You attack " + m1.name + " for " + p1.dmg + " damage");
-                        m1.hp -= p1.dmg;
+                        m1.hpCurrent -= p1.dmg;
                         Console.ForegroundColor = ConsoleColor.Green;
                         //Erase comment mark to show monster health
-                        //Console.WriteLine("It now has " + m1.hp + " health left");
+                        Console.WriteLine("It now has " + m1.hpCurrent + "/" + m1.hpMax + " health left");
                         Console.ForegroundColor = ConsoleColor.Gray;
                         break;
 
@@ -101,7 +104,7 @@ namespace MyTextGame
                 }
                 //checking if the monster is still alive, without this the monster will
                 //deal a final blow with negative health
-                if (m1.hp > 0)
+                if (m1.hpCurrent > 0)
                 {
                     Console.ForegroundColor = ConsoleColor.Red;
                     Console.WriteLine("The " + m1.name + " attacks you for " + m1.dmg + " damage");
@@ -116,7 +119,7 @@ namespace MyTextGame
                         //Now checking if armor is greater than one instead of player class before doing armor calc
                         if (m1.dmg > p1.armor)
                         {
-                            p1.hp -= (m1.dmg - p1.armor);
+                            p1.hpCurrent -= (m1.dmg - p1.armor);
                         }
                         else
                         {
@@ -124,10 +127,10 @@ namespace MyTextGame
                             Console.WriteLine("The attack bounces off your armor");
                         }
                     }
-                    else { p1.hp -= m1.dmg; }
+                    else { p1.hpCurrent -= m1.dmg; }
 
                     Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine("You now have " + p1.hp + " health left \n");
+                    Console.WriteLine("You now have " + p1.hpCurrent + "/" + p1.hpMax + " health left \n");
                     //Console.WriteLine();
                     Console.ForegroundColor = ConsoleColor.Gray;
                 }
@@ -138,10 +141,13 @@ namespace MyTextGame
 
             }
 
-            if (m1.hp <= 0 & p1.hp > 0)
-            { return true; }
+            if (m1.hpCurrent <= 0 & p1.hpCurrent > 0)
+            { Console.WriteLine("The corpse of the fallen " + m1.name + " lies cold at your feet");
+                return true; }
             else
-            { return false; }
+            {
+                Console.WriteLine("Proving superior in battle, the " + m1.name + " has bested you, young " + p1.name);
+                return false; }
         }
 
     }
@@ -155,7 +161,8 @@ namespace MyTextGame
             //making first monster, a large rat
             //set some stats
             Monster m = new Monster();
-            m.hp = 30;
+            m.hpMax = 30;
+            m.hpCurrent = m.hpMax;
             m.minAtk = 1;
             m.maxAtk = 10;
             m.name = "Large Rat";
@@ -182,14 +189,19 @@ namespace MyTextGame
                 switch (classInput)
                 {
                     case "PALADIN":
+                    case "PAL":
+                    case "P":
                         c = new Paladin();
                         validClass = true;
                         break;
                     case "WARRIOR":
+                    case "WAR":
+                    case "W":
                         c = new Warrior();
                         validClass = true;
                         break;
                     case "MAGE":
+                    case "M":
                         c = new Mage();
                         validClass = true;
                         break;
@@ -202,20 +214,13 @@ namespace MyTextGame
                 }
 
             } while (validClass == false);
+            c.hpCurrent = c.hpMax;
 
             //figured out a clever way to utilize the to upper function on chosen class, thought the solution was funny
             //but also figured out how to keep and use the original case, but kept the to upper plot utilization for the development of the npc character
 
             Console.WriteLine("'BUT OF COURSE... YOU WERE A {0}!', exclaims the voice.", classInput);
             //epic pauses to harness the true awkward silence
-            System.Threading.Thread.Sleep(1000);
-            Console.WriteLine("'SHUT UP! OR I'LL BEAT YOU TO SLEEP!', you hear a voice shout in the distance");
-            System.Threading.Thread.Sleep(1000);
-            Console.WriteLine("'...'");
-            System.Threading.Thread.Sleep(1000);
-            Console.WriteLine("'I apologize, I... ");
-            System.Threading.Thread.Sleep(1000);
-            Console.WriteLine("'I have random outbursts when I get excited'");
             System.Threading.Thread.Sleep(1000);
             Console.WriteLine("'Now what did you say your name was, young {0}?'", classInOriginal);
 
@@ -245,7 +250,7 @@ namespace MyTextGame
                 Battle b1 = new Battle();
                 //setting new monsters stats
                 b1.p1 = c; b1.m1 = g;
-                g.hp = 50; g.minAtk = 2; g.maxAtk = 21;
+                g.hpMax = 50; g.hpCurrent = g.hpMax; g.minAtk = 2; g.maxAtk = 21;
                 g.name = "Sleepy Guard";
                 //introducing sleepy guard
                 Console.WriteLine("You see a light in the distance grow, and you watch an angry guard with a torch approach you");
